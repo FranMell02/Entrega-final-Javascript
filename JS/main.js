a84c728fd40af15efa638091129210d8b04db0bb
FranMell02 <137206543+FranMell02@users.noreply.github.com>
2023-10-19 13:59:59 -0300

    V.0.0.8

--- JS/main.js
+++ JS/main.js
@@ -1,136 +1,80 @@
 const content = document.getElementById("content");
-
-const instrumentos = {
-    "instrumento": [
-        {
-            "instrumento": "Guitarra",
-            "marca": "Grestch",
-            "modelo": "Electromatic ",
-            "precio": 2100000,
-            "imagen": "imagenes/grestch.webp"
-        },
-        {
-            "instrumento": "Guitarra",
-            "marca": "Fender",
-            "modelo": "Stratocaster",
-            "precio": 1300000,
-            "imagen": "imagenes/fender.webp"
-        },
-        {
-            "instrumento": "Guitarra",
-            "marca": "Gibson",
-            "modelo": "Les Paul",
-            "precio": 4600000,
-            "imagen": "imagenes/gibson.webp"
-        },
-        {
-            "instrumento": "Guitarra",
-            "marca": "Ibanez",
-            "modelo": "Jem",
-            "precio": 820000,
-            "imagen": "imagenes/ibanez 1.webp"
-        },
-        {
-            "instrumento": "Guitarra",
-            "marca": "Ibanez",
-            "modelo": "Grg 170",
-            "precio": 410000,
-            "imagen": "imagenes/bajo ibanez 2.webp"
-        },
-        {
-            "instrumento": "Bajo",
-            "marca": "Ibanez",
-            "modelo": "Grs206",
-            "precio": 495000,
-            "imagen": "imagenes/bajo ibanez.webp"
-        },
-        {
-            "instrumento": "Bajo",
-            "marca": "Ibanez",
-            "modelo": "Sdgr",
-            "precio": 637000,
-            "imagen": "imagenes/bajo ibanez 2.webp"
-        },
-        {
-            "instrumento": "Bajo",
-            "marca": "Yamaha",
-            "modelo": "Trbx505",
-            "precio": 740000,
-            "imagen": "imagenes/bajo yamaha.webp"
-        },
-        {
-            "instrumento": "Amplificador",
-            "marca": "Marshall",
-            "modelo": "Mg101",
-            "precio": 593000,
-            "imagen": "imagenes/marshall.webp"
-        },
-        {
-            "instrumento": "Amplificador",
-            "marca": "Roland",
-            "modelo": "Jc-40",
-            "precio": 1100000,
-            "imagen": "imagenes/roland.webp"
-        },
-        {
-            "instrumento": "Amplificador",
-            "marca": "Marshall",
-            "modelo": "Code100",
-            "precio": 590000,
-            "imagen": "imagenes/marshall 2.webp"
-        },
-        {
-            "instrumento": "Amplificador",
-            "marca": "Vox",
-            "modelo": "Air",
-            "precio": 500000,
-            "imagen": "imagenes/vox.webp"
-        },
-        {
-            "instrumento": "Bateria",
-            "marca": "Pearl",
-            "modelo": "Roadshow",
-            "precio": 735000,
-            "imagen": "imagenes/pearl.webp"
-        },
-        {
-            "instrumento": "Bateria",
-            "marca": "Pearl",
-            "modelo": "Roadshow silver",
-            "precio": 735000,
-            "imagen": "imagenes/pearl 2.webp"
-        },
-        {
-            "instrumento": "Bateria",
-            "marca": "Rmv",
-            "modelo": "Crossroad",
-            "precio": 360000,
-            "imagen": "imagenes/rmv.webp"
-        },
-        {
-            "instrumento": "Bateria",
-            "marca": "Tama",
-            "modelo": "Rythm Mate",
-            "precio": 740000,
-            "imagen": "imagenes/tama.webp"
-        }
-    ]
-};
+const vercarrito = document.getElementById("carrito");
+const modalcontainer = document.getElementById("modal-container");
 
 let carrito = [];
 
-instrumentos.instrumento.forEach((instrumento) => {
-    let content = document.createElement("div");
-    content.className = "card"
-    content.innerHTML = `
+function crearTarjeta(instrumento) {
+  let card = document.createElement("div");
+  card.className = "card";
+  card.innerHTML = `
     <img src="${instrumento.imagen}">
     <h3>${instrumento.marca} ${instrumento.modelo}</h3>
     <p class="price">$ ${instrumento.precio}</p>
+  `;
+
+  let comprar = document.createElement("button");
+  comprar.innerText = "comprar";
+  card.appendChild(comprar);
+
+  comprar.addEventListener("click", () => {
+    agregarAlCarrito(instrumento);
+  });
+
+  return card;
+}
+
+function agregarAlCarrito(instrumento) {
+  carrito.push({
+    marca: instrumento.marca,
+    modelo: instrumento.modelo,
+    precio: instrumento.precio,
+    imagen: instrumento.imagen,
+  });
+  console.log(carrito);
+}
+
+function mostrarCarritoEnModal() {
+  const modalheader = document.createElement("div");
+  modalheader.className = "modal-header";
+  modalheader.innerHTML = `
+    <h1 class="modal-header-title">Carrito</h1>
+  `;
+
+  modalcontainer.innerHTML = null;
+
+  modalcontainer.append(modalheader);
+
+  const modalbutton = document.createElement("h1");
+  modalbutton.innerText = "X";
+  modalbutton.className = "modal-header-buttons";
+
+  modalheader.append(modalbutton);
+
+  modalbutton.addEventListener("click", () => {
+    modalcontainer.innerHTML = null; 
+  });
+
+  carrito.forEach((instrumento) => {
+    let carritocontent = document.createElement("div");
+    carritocontent.className = "modal-content";
+    carritocontent.innerHTML = `
+        <img src="${instrumento.imagen}">
+        <h3>${instrumento.marca} ${instrumento.modelo}</h3>
+        <p class="price">$ ${instrumento.precio}</p>
     `;
+    modalcontainer.append(carritocontent);
+  });
 
-    let comprar = document.createElement("button");
-    comprar.innerText = "comprar";
-    content.appendChild(comprar);
+  const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+  const compratotal = document.createElement("div");
+  compratotal.className = "total-content";
+  compratotal.innerHTML = `Total a pagar: $ ${total.toLocaleString()}`;
+  modalcontainer.append(compratotal);
+}
 
-    document.getElementById("content").appendChild(content);
+instrumentos.instrumentos.forEach((instrumento) => {
+  content.appendChild(crearTarjeta(instrumento));
 });
+
+vercarrito.addEventListener("click", mostrarCarritoEnModal);
---
+++ JS/productos.js
@@ -0,0 +1,117 @@
+
+const instrumentos = {
+    "instrumentos": [
+        {
+            "instrumento": "Guitarra",
+            "marca": "Grestch",
+            "modelo": "Electromatic ",
+            "precio": 2100000,
+            "imagen": "imagenes/grestch.webp"
+        },
+        {
+            "instrumento": "Guitarra",
+            "marca": "Fender",
+            "modelo": "Stratocaster",
+            "precio": 1300000,
+            "imagen": "imagenes/fender.webp"
+        },
+        {
+            "instrumento": "Guitarra",
+            "marca": "Gibson",
+            "modelo": "Les Paul",
+            "precio": 4600000,
+            "imagen": "imagenes/gibson.webp"
+        },
+        {
+            "instrumento": "Guitarra",
+            "marca": "Ibanez",
+            "modelo": "Jem",
+            "precio": 820000,
+            "imagen": "imagenes/ibanez 1.webp"
+        },
+        {
+            "instrumento": "Guitarra",
+            "marca": "Ibanez",
+            "modelo": "Grg 170",
+            "precio": 410000,
+            "imagen": "imagenes/bajo ibanez 2.webp"
+        },
+        {
+            "instrumento": "Bajo",
+            "marca": "Ibanez",
+            "modelo": "Grs206",
+            "precio": 495000,
+            "imagen": "imagenes/bajo ibanez.webp"
+        },
+        {
+            "instrumento": "Bajo",
+            "marca": "Ibanez",
+            "modelo": "Sdgr",
+            "precio": 637000,
+            "imagen": "imagenes/bajo ibanez 2.webp"
+        },
+        {
+            "instrumento": "Bajo",
+            "marca": "Yamaha",
+            "modelo": "Trbx505",
+            "precio": 740000,
+            "imagen": "imagenes/bajo yamaha.webp"
+        },
+        {
+            "instrumento": "Amplificador",
+            "marca": "Marshall",
+            "modelo": "Mg101",
+            "precio": 593000,
+            "imagen": "imagenes/marshall.webp"
+        },
+        {
+            "instrumento": "Amplificador",
+            "marca": "Roland",
+            "modelo": "Jc-40",
+            "precio": 1100000,
+            "imagen": "imagenes/roland.webp"
+        },
+        {
+            "instrumento": "Amplificador",
+            "marca": "Marshall",
+            "modelo": "Code100",
+            "precio": 590000,
+            "imagen": "imagenes/marshall 2.webp"
+        },
+        {
+            "instrumento": "Amplificador",
+            "marca": "Vox",
+            "modelo": "Air",
+            "precio": 500000,
+            "imagen": "imagenes/vox.webp"
+        },
+        {
+            "instrumento": "Bateria",
+            "marca": "Pearl",
+            "modelo": "Roadshow",
+            "precio": 735000,
+            "imagen": "imagenes/pearl.webp"
+        },
+        {
+            "instrumento": "Bateria",
+            "marca": "Pearl",
+            "modelo": "Roadshow silver",
+            "precio": 735000,
+            "imagen": "imagenes/pearl 2.webp"
+        },
+        {
+            "instrumento": "Bateria",
+            "marca": "Rmv",
+            "modelo": "Crossroad",
+            "precio": 360000,
+            "imagen": "imagenes/rmv.webp"
+        },
+        {
+            "instrumento": "Bateria",
+            "marca": "Tama",
+            "modelo": "Rythm Mate",
+            "precio": 740000,
+            "imagen": "imagenes/tama.webp"
+        }
+    ]
+};
